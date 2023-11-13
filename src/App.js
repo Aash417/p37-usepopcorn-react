1e0219116c8bf2f1f62ba2d2da956adadf51f651
aashish_k <aashishkathat00@gmail.com>
2023-11-13 22:14:22 +0530

    using composition to make a resuable box

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const average = (arr) =>
 
 export default function App() {
 	const [movies, setMovies] = useState(tempMovieData);
+	const [watched, setWatched] = useState(tempWatchedData);
 
 	return (
 		<>
@@ -60,10 +61,13 @@ export default function App() {
 				<NumResults movies={movies} />
 			</NavBar>
 			<Main>
-				<ListBox>
+				<Box>
 					<MovieList movies={movies} />
-				</ListBox>
-				<WatchedBox />
+				</Box>
+				<Box>
+					<WatchedSummary watched={watched} />
+					<WatchedMovieList watched={watched} />
+				</Box>
 			</Main>
 		</>
 	);
@@ -112,17 +116,14 @@ function Main({ children }) {
 	return <main className="main">{children}</main>;
 }
 //...................................
-function ListBox({ children }) {
-	const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+	const [isOpen, setIsOpen] = useState(true);
 	return (
 		<div className="box">
-			<button
-				className="btn-toggle"
-				onClick={() => setIsOpen1((open) => !open)}
-			>
-				{isOpen1 ? "–" : "+"}
+			<button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+				{isOpen ? "–" : "+"}
 			</button>
-			{isOpen1 && children}
+			{isOpen && children}
 		</div>
 	);
 }
@@ -152,6 +153,7 @@ function Movie({ movie }) {
 	);
 }
 ///////////////////////////////////////
+/*
 function WatchedBox() {
 	const [watched, setWatched] = useState(tempWatchedData);
 	const [isOpen2, setIsOpen2] = useState(true);
@@ -173,6 +175,8 @@ function WatchedBox() {
 		</div>
 	);
 }
+*/
+
 function WatchedSummary({ watched }) {
 	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
 	const avgUserRating = average(watched.map((movie) => movie.userRating));
