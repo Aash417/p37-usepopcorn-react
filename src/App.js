5ec896cd2bfcdb9a6295f74fde33e0764fe7654f
aashish_k <aashishkathat00@gmail.com>
2023-11-18 21:14:35 +0530

    deleting a watched movie

--- src/App.js
+++ src/App.js
@@ -75,6 +75,10 @@ export default function App() {
 		setWatched((watched) => [...watched, movie]);
 	}
 
+	function handleDeleteWatched(id) {
+		setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+	}
+
 	useEffect(
 		function () {
 			async function fetchMovies() {
@@ -148,7 +152,10 @@ export default function App() {
 					) : (
 						<>
 							<WatchedSummary watched={watched} />
-							<WatchedMovieList watched={watched} />
+							<WatchedMovieList
+								watched={watched}
+								onDeleteWatched={handleDeleteWatched}
+							/>
 						</>
 					)}
 				</Box>
@@ -394,11 +401,11 @@ function WatchedSummary({ watched }) {
 				</p>
 				<p>
 					<span>⭐️</span>
-					<span>{avgImdbRating}</span>
+					<span>{avgImdbRating.toFixed(2)}</span>
 				</p>
 				<p>
 					<span>🌟</span>
-					<span>{avgUserRating}</span>
+					<span>{avgUserRating.toFixed(2)}</span>
 				</p>
 				<p>
 					<span>⏳</span>
@@ -409,17 +416,21 @@ function WatchedSummary({ watched }) {
 	);
 }
 
-function WatchedMovieList({ watched }) {
+function WatchedMovieList({ watched, onDeleteWatched }) {
 	return (
 		<ul className="list">
 			{watched.map((movie) => (
-				<WatchedMoive movie={movie} key={movie.imdbID} />
+				<WatchedMoive
+					movie={movie}
+					key={movie.imdbID}
+					onDeleteWatched={onDeleteWatched}
+				/>
 			))}
 		</ul>
 	);
 }
 
-function WatchedMoive({ movie }) {
+function WatchedMoive({ movie, onDeleteWatched }) {
 	return (
 		<li key={movie.imdbID}>
 			<img src={movie.poster} alt={`${movie.title} poster`} />
@@ -437,6 +448,13 @@ function WatchedMoive({ movie }) {
 					<span>⏳</span>
 					<span>{movie.runtime} min</span>
 				</p>
+
+				<button
+					className="btn-delete"
+					onClick={() => onDeleteWatched(movie.imdbID)}
+				>
+					X
+				</button>
 			</div>
 		</li>
 	);
