21fd2cbc7c0b8a1cf202330c10b24d5bacd73498
aashish_k <aashishkathat00@gmail.com>
2023-11-18 19:12:03 +0530

    selecting a movie

--- src/App.js
+++ src/App.js
@@ -58,8 +58,17 @@ export default function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState();
 	const [query, setQuery] = useState("");
+	const [selectedId, setSelectedId] = useState(null);
 
 	const tempQuery = "batman";
+
+	function handleSelectMovie(id) {
+		setSelectedId((selectedId) => (id === selectedId ? null : id));
+	}
+
+	function handleCloseMovie() {
+		setSelectedId(null);
+	}
 
 	useEffect(
 		function () {
@@ -118,12 +127,23 @@ export default function App() {
 					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
 
 					{isLoading && <Loader />}
-					{!isLoading && !error && <MovieList movies={movies} />}
+					{!isLoading && !error && (
+						<MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+					)}
 					{error && <ErrorMessage message={error} />}
 				</Box>
 				<Box>
-					<WatchedSummary watched={watched} />
-					<WatchedMovieList watched={watched} />
+					{selectedId ? (
+						<MovieDetails
+							selectedId={selectedId}
+							onCloseMovie={handleCloseMovie}
+						/>
+					) : (
+						<>
+							<WatchedSummary watched={watched} />
+							<WatchedMovieList watched={watched} />
+						</>
+					)}
 				</Box>
 			</Main>
 		</>
@@ -195,19 +215,19 @@ function Box({ children }) {
 	);
 }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
 	return (
-		<ul className="list">
+		<ul className="list list-movies">
 			{movies?.map((movie) => (
-				<Movie movie={movie} key={movie.imdbID} />
+				<Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
 			))}
 		</ul>
 	);
 }
 
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
 	return (
-		<li key={movie.imdbID}>
+		<li key={movie.imdbID} onClick={() => onSelectMovie(movie.imdbID)}>
 			<img src={movie.Poster} alt={`${movie.Title} poster`} />
 			<h3>{movie.Title}</h3>
 			<div>
@@ -217,6 +237,17 @@ function Movie({ movie }) {
 				</p>
 			</div>
 		</li>
+	);
+}
+
+function MovieDetails({ selectedId, onCloseMovie }) {
+	return (
+		<div className="details">
+			<button className="btn-back" onClick={onCloseMovie}>
+				&larr;
+			</button>
+			{selectedId}
+		</div>
 	);
 }
 ///////////////////////////////////////
