0888935770537f48e6acca6ff50093a63bbef003
aashish_k <aashishkathat00@gmail.com>
2023-11-19 23:56:33 +0530

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./starRating";
+import { useMovies } from "./useMovies";
 
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -7,9 +8,6 @@ const average = (arr) =>
 const KEY = "134a2767";
 
 export default function App() {
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
 	const [query, setQuery] = useState("");
 	const [selectedId, setSelectedId] = useState(null);
 	// const [watched, setWatched] = useState([]);
@@ -19,6 +17,8 @@ export default function App() {
 		if (storedValue) return JSON.parse(storedValue);
 		else return [];
 	});
+
+	const { movies, isLoading, error } = useMovies(query);
 
 	// const tempQuery = "batman";
 
@@ -45,49 +45,6 @@ export default function App() {
 		[watched]
 	);
 
-	useEffect(
-		function () {
-			const controller = new AbortController();
-
-			async function fetchMovies() {
-				try {
-					setIsLoading(true);
-					setError("");
-					const res = await fetch(
-						`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-						{ signal: controller.signal }
-					);
-
-					if (!res.ok)
-						throw new Error("something went wrong with fetching movies.");
-
-					const data = await res.json();
-					if (data.Response === "False") throw new Error("Movie not found.!");
-
-					setMovies(data.Search);
-					setError("");
-				} catch (err) {
-					if (err.name !== "AbortError") setError(err.message);
-				} finally {
-					setIsLoading(false);
-				}
-			}
-
-			if (query.length < 3) {
-				setMovies([]);
-				setError("");
-				return;
-			}
-			handleCloseMovie();
-			fetchMovies();
-
-			return function () {
-				controller.abort();
-			};
-		},
-		[query]
-	);
-
 	return (
 		<>
 			<NavBar movies={movies}>
@@ -95,19 +52,7 @@ export default function App() {
 				<NumResults movies={movies} />
 			</NavBar>
 			<Main>
-				{/* <Box element={<MovieList movies={movies} />} />
-
-				<Box
-					element={
-						<>
-							<WatchedSummary watched={watched} />
-							<WatchedMovieList watched={watched} />
-						</>
-					}
-				/> */}
 				<Box>
-					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-
 					{isLoading && <Loader />}
 					{!isLoading && !error && (
 						<MovieList movies={movies} onSelectMovie={handleSelectMovie} />
