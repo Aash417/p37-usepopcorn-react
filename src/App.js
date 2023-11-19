8d56d1911044a3a92d661be40a156f16fcedc3f7
aashish_k <aashishkathat00@gmail.com>
2023-11-19 21:25:56 +0530

    refs to persists data b/w render

--- src/App.js
+++ src/App.js
@@ -253,6 +253,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 	const [movie, setMovie] = useState({});
 	const [isLoading, setIsLoading] = useState(false);
 	const [userRating, setUserRating] = useState("");
+
+	const countRef = useRef(0);
+	useEffect(
+		function () {
+			if (userRating) countRef.current = countRef.current += 1;
+		},
+		[userRating]
+	);
+
 	const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
 	const watchedUserRating = watched.find(
 		(movie) => movie.imdbID === selectedId
@@ -282,6 +291,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 			imdbRating: Number(imdbRating),
 			runtime: Number(runtime.split(" ").at(0)),
 			userRating,
+			countRatingDecision: countRef.current,
 		};
 		// console.log(newWatchedMovie);
 
